c66581e96c732ffb1c4e84a245de74ead61cbdf6
tomeytran <tmtri310251@gmail.com>
2024-10-09 14:35:00 +0700

    Calculate the rectangle area based on width and height input

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class main {
+public class Rectangle {
     public static void main(String[] args) {
         float width;
         float height;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter width: ");
-        width = scanner.nextFloat(); // ~prompt.value
+        width =scanner.nextFloat(); // ~prompt.value
         System.out.println("Enter height: ");
-        height = scanner.nextFloat();
+        height =scanner.nextFloat();
 
         float area = width * height;
-        System.out.println("Area is " + area);
+        System.out.println("Area is "+area);
     }
+
 }
